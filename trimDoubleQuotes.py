b01200f3509cf65567963f094c6c73dc8cabbcb9
vertisinfotechakash <vertisinfotechakash@ooyala.com>
2016-11-30 12:44:04 +0530

    wrapper for trimming double quotes

---
+++ trimDoubleQuotes.py
@@ -0,0 +1,10 @@
+def trunc(arr=[], *args):
+	for x in arr:
+		print(",".join(x))
+
+/*
+
+The above function is used to trim all the double quotes available in the list 
+arr =  [["2","#","4"], ["546","456","67"]]
+trunc(arr);
+*/
